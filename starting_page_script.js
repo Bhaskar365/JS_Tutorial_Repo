675c6eaa4b58206b1c0705dced21e382f7756251
Bhaskar <bhaskarjyotidas365@gmail.com>
2023-06-04 19:13:46 +0530

    new example added. Work in progress

--- starting_page_script.js
+++ starting_page_script.js
@@ -1,34 +1,37 @@
-const country = 'India';
-const continent = 'Asia';
-let population = 1300000000;
+// const country = 'India';
+// const continent = 'Asia';
+// let population = 1300000000;
 
-console.log(country);
-console.log(continent);
-console.log(population);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
 
-let isIsland = new Boolean();
-isIsland = false;
-
-let language;
-language = 'english';
-
-console.log(isIsland);
-console.log(language);
+// let isIsland = new Boolean();
+// let isBool = false;
 
 
-if(population > 33000000){
-    console.log('population is above 33000000' );
-}
-else{
-    console.log('population is below 330000000');
-}
+// isIsland = false;
+
+// let language;
+// language = 'english';
+
+// console.log(isIsland);
+// console.log(language);
 
 
-console.log('9'-'5');
-console.log('19'-'13'+'17');
-console.log('19'-'13'+17);
-console.log('123'<57);
-console.log(5 + 6 + '4' + 9 - 4 - 2);
+// if(population > 33000000){
+//     console.log('population is above 33000000' );
+// }
+// else{
+//     console.log('population is below 330000000');
+// }
+
+
+// console.log('9'-'5');
+// console.log('19'-'13'+'17');
+// console.log('19'-'13'+17);
+// console.log('123'<57);
+// console.log(5 + 6 + '4' + 9 - 4 - 2);
 
 // let numNeighbours;
 // numNeighbours = prompt('How many neighbour countries  does your country have?');
@@ -54,46 +57,82 @@ console.log(5 + 6 + '4' + 9 - 4 - 2);
 //     alert('country not in criteria');
 // }
 
-let input = prompt('Enter language');
-switch(input)
-{
-    case 'chinese':
-        alert('most number of native speaker');
-        break;
+// let input = prompt('Enter language');
+// switch(input)
+// {
+//     case 'chinese':
+//     case 'mandarin':
+//         alert('most number of native speaker');
+//         break;
 
-    case 'mandarin':
-         alert('most number of native speaker');
-        break;
-
-    case 'spanish':
-        alert('2nd place in number of native speakers');
-        break;
+//     case 'spanish':
+//         alert('2nd place in number of native speakers');
+//         break;
     
-    case 'english':
-        alert('3nd place in number of native speakers');
-        break;
+//     case 'english':
+//         alert('3nd place in number of native speakers');
+//         break;
 
-    case 'hindi':
-        alert('4th place in number of native speakers');
-        break;    
+//     case 'hindi':
+//         alert('4th place in number of native speakers');
+//         break;    
 
-    case 'arabic':
-        alert('5th most spoken language');
-        break;
+//     case 'arabic':
+//         alert('5th most spoken language');
+//         break;
 
-    default:
-        alert('invalid language entered');    
-        break;
-}
+//     default:
+//         alert('invalid language entered');    
+//         break;
+// }
 
+// if(population>3000000){
+//     // alert('population is above 30000000');
+// }
+// else{
+//     // alert('population is below 30000000');
+// }
 
+// const desc = `${country} describes population ${population} speaking ${language}`;
+// // alert(desc);
+// console.log(typeof isBool);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
 
+// // const numNeighbours = Number(prompt('How many neighbours does country have?'));
 
+// if(numNeighbours===1)
+// {
+//     alert('Only 1 border');
+// }
+// else if(numNeighbours >1)
+// {
+//     alert('more than 1 border');
+// }
+// else
+// {
+//     alert('no borders');
+// }
 
+// alert(`${country}'s population is ${population > 330000 ? 'above' : 'below'} average`);
 
+const ageJonas = 2033-1999;
+const ageSarah = 2033-2004;
+ 
+console.log(ageJonas,ageSarah);
 
+let x ,y;
+x=y=25-5-2;
+console.log(x , y);
 
+// const john = 'teacher';
+// const w = `I'm ${john}`;
+// alert(w);
 
+alert('String with \n\
+ multiple \n\
+ lines  ');
 
 
 
